88d890e04aa6f8de09c4c339813abb9ab32a3c1e
DexGroves <dex.groves@gmail.com>
2015-11-19 18:47:56 +0000

    Make classes neater, add basic quantile cutting

--- R/DataMunger.r
+++ R/DataMunger.r
@@ -1,29 +1,3 @@
-initialize <- function(input_df, plot_cols, by_col) {
-  self$plot_cols <- plot_cols
-  self$by_col <- by_col
-  self$vb <- VectorBucketer$new()
-  self$df <- self$generate_core_df(input_df)
-}
-
-generate_core_df <- function(input_df) {
-  data.table(input_df[, c(self$by_col, self$plot_cols)])
-}
-
-bucket_data <- function(buckets) {
-  self$df[, grp_by_col := self$vb$cut_evenly(get(self$by_col), buckets)]
-  self$bucketed_df <- self$df[, lapply(.SD, mean),
-                              .SDcols = self$plot_cols, by = grp_by_col]
-  invisible()
-}
-
-melt_df <- function() {
-  self$melted_df <- melt(self$bucketed_df, id.vars = "grp_by_col")
-  setkey(self$melted_df, grp_by_col)
-}
-
-#' @import R6
-#' @import data.table
-#' @import reshape2
 DataMunger <- R6Class("DataMunger",
   public = list(
     plot_cols = NA,
@@ -32,19 +6,64 @@ DataMunger <- R6Class("DataMunger",
     vb = NA,
     bucketed_df = NA,
     melted_df = NA,
-    initialize = initialize,
-    generate_core_df = generate_core_df,
-    bucket_data = bucket_data,
-    melt_df = melt_df))
+
+    initialize = function(input_df, plot_cols, by_col, cut_type) {
+      self$plot_cols <- plot_cols
+      self$by_col <- by_col
+      self$vb <- VectorBucketer$new(cut_type)
+      self$df <- self$generate_core_df(input_df)
+    },
+
+    generate_core_df = function(input_df) {
+      data.table(input_df[, c(self$by_col, self$plot_cols)])
+    },
 
 
-cut_evenly <- function(cut_vector, buckets) {
-  lower <- min(cut_vector)
-  upper <- max(cut_vector)
-  cut_points <- seq(lower, upper, length.out = buckets)
-  cut(cut_vector, breaks = cut_points, include.lowest = TRUE)
-}
+    bucket_data = function(buckets) {
+      self$df[, grp_by_col := self$vb$cut_vector(get(self$by_col), buckets)]
+      self$bucketed_df <- self$df[, lapply(.SD, mean),
+                                  .SDcols = self$plot_cols, by = grp_by_col]
+      invisible()
+    },
+
+    melt_df = function() {
+      self$melted_df <- melt(self$bucketed_df, id.vars = "grp_by_col")
+      setkey(self$melted_df, grp_by_col)
+    }
+  )
+)
 
 VectorBucketer <- R6Class("VectorBucketer",
   public = list(
-    cut_evenly = cut_evenly))
+    cut_type = NA,
+
+    initialize = function(cut_type) {
+      self$cut_type <- cut_type
+    },
+
+    cut_vector = function(cut_vector, buckets) {
+      if (self$cut_type == "even") {
+        return(self$cut_evenly(cut_vector, buckets))
+      }
+      if (self$cut_type == "quantile") {
+        return(self$cut_evenly(cut_vector, buckets))
+      }
+      else {
+        stop("cut type not recognised!", call. = FALSE)
+      }
+    },
+
+    cut_evenly = function(cut_vector, buckets) {
+      lower <- min(cut_vector)
+      upper <- max(cut_vector)
+      cut_points <- seq(lower, upper, length.out = buckets)
+      cut(cut_vector, breaks = cut_points, include.lowest = TRUE)
+    },
+
+    cut_by_quantile = function(cut_vector, buckets) {
+      quantiles <- seq(0, 1, length.out = buckets)
+      cut_points <- quantile(cut_vector, probs = quantiles)
+      cut(cut_vector, breaks = cut_points, include.lower = TRUE)
+    }
+  )
+)
--- R/universe.R
+++ R/universe.R
@@ -1,6 +1,10 @@
 #' @import plotly
 #' @import magrittr
-plot_df <- function(dm, by_col) {
+#' @import R6
+#' @import data.table
+#' @import reshape2
+
+plot_df <- function(dm) {
   ay <- list(
     tickfont = list(color = "red"),
     overlaying = "y",
@@ -17,9 +21,10 @@ plot_df <- function(dm, by_col) {
 universe <- function(input_df,
                      plot_cols,
                      by_col,
-                     buckets = 10) {
-  dm <- DataMunger$new(input_df, plot_cols, by_col)
+                     buckets = 10,
+                     cut_type = "even") {
+  dm <- DataMunger$new(input_df, plot_cols, by_col, cut_type)
   dm$bucket_data(buckets)
   dm$melt_df()
-  plot_df(dm, by_col)
+  plot_df(dm)
 }
